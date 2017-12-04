5c3d78e1798e3403e4b74fa8201e6d152735132d
ytois <ooishi@rejob.co.jp>
2017-12-04 10:38:32 +0900

    add stage table

--- models/__init__.py
+++ models/__init__.py
@@ -5,3 +5,4 @@ db = SQLAlchemy(current_app)
 
 from models.user import User
 from models.switch_session import SwitchSession
+from models.stage import Stage
---
+++ models/stage.py
@@ -0,0 +1,34 @@
+from models import db
+from datetime import datetime
+
+
+class Stage(db.Model):
+    __tablename__ = 'stages'
+
+    id = db.Column(db.Integer, primary_key=True)
+    stage_id = db.Column(db.Integer, nullable=False)
+    name = db.Column(db.String, nullable=False)
+    image = db.Column(db.String)
+    created_at = db.Column(db.DateTime, nullable=False, default=datetime.utcnow)
+    updated_at = db.Column(db.DateTime, onupdate=datetime.utcnow)
+
+    def add(self):
+        db.session.add(self)
+        return self
+
+    def commit(self):
+        return db.session.commit()
+
+    @classmethod
+    def save_from_dict(self, _dict):
+        if Stage.query.filter_by(stage_id=int(_dict['id'])).first():
+            return None
+
+        stage = Stage(
+            id=(1 + int(_dict['id'])),
+            stage_id=int(_dict['id']),
+            name=_dict['name'],
+            image=_dict['image'],
+        )
+
+        return stage.add().commit()
