c86f183a63aa5a59cac3cb34b1cf2421b87625a5
ytois <ooishi@rejob.co.jp>
2017-12-09 12:14:40 +0900

    Vue単一ファイルコンポーネント化

---
+++ assets/scripts/components/latest_result.vue
@@ -0,0 +1,35 @@
+<template lang="pug">
+  div.uk-card.uk-card-body.uk-card-default
+    div(class='uk-child-width-expand@s' uk-grid='')
+      h2 latest
+      .uk-text-right
+        p 2017-12-01 12:00:00
+    div(uk-grid='')
+      div.uk-width-medium
+        img(src='/static/img/gangaze.jpg')
+      div
+        p ナワバリバトル
+        p モズク農園
+        p WIN! 60%
+    progress.uk-progress(value="60" max="100")
+    div.uk-grid-divider(class='uk-child-width-expand@s' uk-grid='')
+      div
+        p S+ weapon   ぞうさん(sort)      5k(2) 3d
+        p S+ てぃけぽまる
+        p S+ チャンコーリー
+        p S+ たきぽまる
+      div
+        p ほのお
+        p ひざ
+        p take
+        p ひげなっくる
+</template>
+
+<style lang="stylus">
+</style>
+
+<script lang="coffee">
+module.exports =
+  data: ->
+    message: ""
+</script>
--- assets/scripts/entry.js.coffee
+++ assets/scripts/entry.js.coffee
@@ -1,3 +1,11 @@
 window.UIkit = require('uikit')
 window.UIkitIcons = require 'uikit/dist/js/uikit-icons'
 
+Vue = require 'vue'
+latest_result= require './components/latest_result.vue'
+
+new Vue(
+  el: '#latest_result'
+  components:
+    'latest-result': latest_result
+)
